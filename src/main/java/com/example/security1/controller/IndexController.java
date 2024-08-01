3093371d1142c919c035b3a2ea186d58dc02fdc0
hayeongKo <polarpheno@icloud.com>
2024-08-01 18:12:05 +0900

    [FEAT] 회원가입 로직 구현

--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -1,10 +1,22 @@
 package com.example.security1.controller;
 
+import com.example.security1.model.User;
+import com.example.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //view를 리턴하겠다
 public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping({"", "/"})
     public String index() {
         //mustache 기본폴더 src/main/resources/
@@ -12,4 +24,46 @@ public class IndexController {
         //자동으로 경로가 기본적으로 잡힘 -> 생략가능
         return "index"; //src/main/resources/templates/index.mustache 로 경로 탐색하게 됨 -> .html을 못찾음
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // spring security가 먼저 낚아챔 -> securityConfig 파일 생성 후 작동 X
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "loginForm";
+//    }
+
+    @PostMapping("/join")
+    public @ResponseBody String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user); //회원가입은 잘되는데 비밀번호: 1234 -> 시큐리티로 로그인을 할 수 없음.
+        //이유는 패스워드가 암호화가 안되었기 때문
+        return "redirect:/loginForm"; //loginForm이라는 함수를 호출해줌
+    }
+
 }
