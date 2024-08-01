3fc52fb8b21b64847459e634c55cc89a7c0c587d
hayeongKo <polarpheno@icloud.com>
2024-08-01 18:11:56 +0900

    [FEAT] user 모델 생성 및 jpa repository 생성

---
+++ src/main/java/com/example/security1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.security1.repository;
+
+import com.example.security1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//CRUD 함수를 JPARepository가 들고 있음
+//@Repository라는 어노테이션이 없어도 IoC됨. 왜냐 JpaRepository를 상속받았기 때문
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
