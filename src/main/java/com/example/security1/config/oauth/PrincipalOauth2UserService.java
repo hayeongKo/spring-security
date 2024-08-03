25d97e5e754917b633f07a2c119ff32865bd023d
hayeongKo <polarpheno@icloud.com>
2024-08-03 14:01:08 +0900

    [CHORE] OAuth 유저 주석 추가

--- src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
@@ -11,10 +11,15 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     // 구글로부터 받은 userRequest 데이터에 대한 후처리되는 함수
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        //registerationId로 어떤 OAuth로 로그인 했는지 확인가능
         System.out.println("getClientRegistration: " + oAuth2UserRequest.getClientRegistration());
         // 사실 엑세스토큰이 없어도 아래에서 getAttributes에서 사용자 정보를 모두 받아올 수 있어서 딱히 필요하지 않음
         System.out.println("getAccessToken: " + oAuth2UserRequest.getAccessToken().getTokenValue());
+        // 구글 로그인 버튼 클릭 -> 구글로그인 창 -> 로그인 완료 -> code 리턴(OAuth-Client 라이브러리) -> Access Token 요청
+        // userRequest 정보 -> loadUser 함수 호출 -> 구글로부터 회원프로필을 받아줌.
         System.out.println("getAttributes: " + super.loadUser(oAuth2UserRequest).getAttributes());
+
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
         return super.loadUser(oAuth2UserRequest);
     }
 }
